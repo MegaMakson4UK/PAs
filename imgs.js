f247e0cf2750fb21f81db01b510531077e470e5b
MegaMakson4UK <maximp2235@gmail.com>
2024-12-29 14:16:43 +0200

    link edit

--- imgs.js
+++ imgs.js
@@ -1,3 +1,3 @@
-const img1 = ''
-const img2 = ''
-const img3 = ''
+const img1 = 'https://raw.githubusercontent.com/MegaMakson4UK/PAs/PA3/grip_steel_normal.png'
+const img2 = 'https://raw.githubusercontent.com/MegaMakson4UK/PAs/PA3/grip_steel_specular.png'
+const img3 = 'https://raw.githubusercontent.com/MegaMakson4UK/PAs/PA3/grip_steel_texture.jpg'
